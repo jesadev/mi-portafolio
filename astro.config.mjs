6881be60ee481ae7b0c51d467a8e17009be31115
gensanchez <genesis.sanchez2009@gmail.com>
2026-03-10 21:39:22 -0400

    setup astro base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tu-portfolio.vercel.app',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
